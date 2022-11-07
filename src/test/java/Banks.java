d61791815360af42101cd0b3633df6cf26c7e4ef
SukreetiSharma <sukreeti.sharma@infostride.com>
2022-11-07 06:22:14 -0800

    Convert Xml to String

---
+++ src/test/java/Banks.java
@@ -0,0 +1,29 @@
+public class Banks {
+    private int id;
+    private String name;
+    private String headquarter;
+
+    public String getHeadquarter() {
+        return headquarter;
+    }
+
+    public void setHeadquarter(String headquarter) {
+        this.headquarter = headquarter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ src/test/java/BanksTest.java
@@ -0,0 +1,18 @@
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BanksTest {
+    public static void main(String args[]) throws Exception {
+        XmlMapper xml = new XmlMapper();
+        String readContent = new String(Files.readAllBytes(Paths.get("C:\\Users\\SukreetiSharma\\Documents\\GitHub\\Serialization_Task\\src\\test\\java\\Data.xml")));
+        Banks BanksData = xml.readValue(readContent, Banks.class);
+        int Id = BanksData.getId();
+        String Name = BanksData.getName();
+        String Headquarter = BanksData.getHeadquarter();
+        System.out.println("ID = " + Id);
+        System.out.println("Name = " + Name);
+        System.out.println("Headquarter = " + Headquarter);
+    }
+
+}
